638cfaea69d7f3c229c167c60863369b96d5a14d
Celnik-213 <uT_4000@zsp3zamosc.pl>
2026-05-29 10:09:46 +0200

    pogoda

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import UserProfile from './components/UserProfile'
+import WeatherCard from './components/WeatherCard'
 import './App.css'
 
 function App() {
@@ -17,6 +18,12 @@ function App() {
         website="https://jan-kowalski.example.com" 
         isPremium={true} 
         followerCount={1000} 
+      />
+      <WeatherCard 
+        city="Warszawa" 
+        temperature={25} 
+        conditions="sunny" 
+        warning="Uwaga! Możliwe burze w godzinach popołudniowych." 
       />
     </>
   )
